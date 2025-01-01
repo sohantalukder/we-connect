33901da74f034355d3c6872aa2531c3dc9acb76f
sohantalukder <sohan15-1933@diu.edu.bd>
2025-01-01 19:34:00 +0600

    add mobile number constant

--- we-connect-app/src/app/modules/chats/index.tsx
+++ we-connect-app/src/app/modules/chats/index.tsx
@@ -2,12 +2,15 @@ import {Text} from 'react-native';
 import React from 'react';
 import MainHeader from '@components/header/MainHeader.c';
 import Container from '@layouts/Container.layout';
-
+import mobileNumberConstant from '@constants/mobileNumberConstant';
 const ChatsIndex = () => {
   return (
     <Container>
       <MainHeader title={'Chats'} />
       <Text>ChatsIndex</Text>
+      <Text style={{color: 'white', fontSize: 300}}>
+        {mobileNumberConstant[0].icon}
+      </Text>
     </Container>
   );
 };
